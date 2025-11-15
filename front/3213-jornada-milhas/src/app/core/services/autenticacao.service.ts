383540cb5becd6afc437f6355fef02031186c4a0
guiziis <guigamoda@hotmail.com>
2025-11-15 16:57:16 -0300

    feat add auth service front

--- front/3213-jornada-milhas/src/app/core/services/autenticacao.service.ts
+++ front/3213-jornada-milhas/src/app/core/services/autenticacao.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 
 @Injectable({
@@ -9,7 +10,7 @@ export class AutenticacaoService {
   private readonly http = inject(HttpClient);
   private apiUrl = environment.apiUrl;
 
-  autenticar(email: string, password: string) {
-    return this.http.post()
+  autenticar(email: string, senha: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/auth/login`, {email, senha});
   }
 }
--- front/3213-jornada-milhas/src/app/pages/login/login.component.ts
+++ front/3213-jornada-milhas/src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { catchError, first, of } from 'rxjs';
+import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -7,18 +10,34 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  private readonly autenticacaoService = inject(AutenticacaoService);
+  private readonly router = inject(Router);
   private formBuilder = new FormBuilder();
-  loginForm!: FormGroup;
+  public loginForm!: FormGroup;
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: [null],
-      password: [null]
+      senha: [null]
     });
   }
 
 
   login() {
-    console.log('login fig')
-   }
+    const { email, senha } = this.loginForm.value;
+
+    this.autenticacaoService
+      .autenticar(email, senha)
+      .pipe(
+        first(),
+        catchError((error) => {
+          console.error('login error', error)
+          return of(null);
+        })
+      )
+      .subscribe((response) => {
+        this.router.navigate(['/']);
+        console.log('login response', response);
+      });
+  }
 }
